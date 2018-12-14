afa1653ce76a88171b13935e1cb8a262f8b38a4f
Minyoung Heo <minyoungheo.heo@gmail.com>
2018-12-14 13:39:31 -0800

    ImageGrabber can handle sprites

--- src/com/miniminja/ImageManipulator/ImageGrabber.java
+++ src/com/miniminja/ImageManipulator/ImageGrabber.java
@@ -17,4 +17,17 @@ public class ImageGrabber {
 		}
 		return ret;
 	}
+	
+	public static BufferedImage[] getAnimation(String filePath, int rows, int cols, int width, int height, int frames) {
+		BufferedImage[] ret = new BufferedImage[frames];
+		BufferedImage sprite = getImage(filePath);
+		int frameIndex = 0;
+		for(int i = 0;i<rows;i++) {
+			for(int j = 0;j<cols;j++) {
+				ret[frameIndex++] = sprite.getSubimage(j*width, i*height, width, height);
+				if(frameIndex == frames) return ret;
+			}
+		}
+		return ret;
+	}
 }
